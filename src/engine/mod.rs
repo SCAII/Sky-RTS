040d3814731132daaac0020b475623183d6a7a00
Zoe Juozapaitis <Jragonmiris@gmail.com>
2017-10-19 21:35:55 -0700

    Removed spurious src directory

--- src/engine/mod.rs
+++
@@ -1,8 +0,0 @@
-mod entity;
-mod map;
-
-#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
-pub struct Pos {
-    x: usize,
-    y: usize,
-}
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![allow(dead_code)]
-
-extern crate ndarray;
-extern crate rlua;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-
-mod engine;
