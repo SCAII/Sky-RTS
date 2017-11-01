e898f9c8a4ad9f2a5cd63392ebbf1f161e0e5f9e
Zoe Juozapaitis <Jragonmiris@gmail.com>
2017-10-31 22:27:50 -0700

    Basic restart and update sketches

--- backend/src/engine/mod.rs
+++ backend/src/engine/mod.rs
@@ -5,6 +5,11 @@ pub mod systems;
 use scaii_defs::protos::MultiMessage;
 use specs::World;
 
+#[derive(Copy, Clone, PartialEq, Eq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+pub struct Episode(usize);
+#[derive(Copy, Clone, PartialEq, Eq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
+pub struct Terminal(bool);
+
 pub struct Rts {
     world: World,
 }
@@ -17,8 +22,11 @@ impl Rts {
         let mut world = World::new();
         world.register::<self::components::Pos>();
         world.register::<self::components::Heading>();
+
         let rng = util::no_fail_std_rng();
         world.add_resource(rng);
+        world.add_resource(Episode(0));
+        world.add_resource(Terminal(false));
 
         Rts { world }
     }
@@ -36,10 +44,31 @@ impl Rts {
     }
 
     pub fn restart(&mut self) -> MultiMessage {
+        use rand::StdRng;
+        use util;
+
+        self.world.delete_all();
+        // Do a fast reseed so it doesn't start looping the RNG state
+        // after too many episodes
+        {
+            let rng = &mut *self.world.write_resource::<StdRng>();
+            util::diverge_std_rng_fast(rng);
+
+            self.world.write_resource::<Episode>().0 += 1;
+            self.world.write_resource::<Terminal>().0 = false;
+        }
+
+        self.world.maintain();
+
         unimplemented!()
     }
 
     pub fn update(&mut self) -> MultiMessage {
+        if self.world.read_resource::<Terminal>().0 {
+            return Default::default();
+        }
+
+        self.world.maintain();
         unimplemented!()
     }
 }
